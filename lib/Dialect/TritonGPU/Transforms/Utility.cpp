e90d52fd1bf509ec9c4a42623f552f7d4802ce55
Shucai Xiao <shucai.xiao@amd.com>
2026-08-11 23:35:44 -0500

    backup kernel changes with explicit divisibility

--- lib/Dialect/TritonGPU/Transforms/Utility.cpp
+++ lib/Dialect/TritonGPU/Transforms/Utility.cpp
@@ -196,7 +196,8 @@ unsigned getNumElementsPerThread(Operation *op, SmallVector<unsigned> order,
                         << ", divisibility: " << maxMultipleBytes
                         << ", contig: " << valInfo.getContiguity(order[0])
                         << ", maximum: " << maxElementsPerThread
-                        << ", alignment: " << alignment);
+                        << ", alignment: " << alignment
+                        << ", maxContig: " << maxContig);
   return currPerThread;
 }
 
